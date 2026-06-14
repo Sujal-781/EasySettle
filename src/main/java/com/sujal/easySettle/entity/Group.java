ef9792d77a9ff3d0d16df76630d40b6321e62320
Sujal Choudhary <chsujal8@gmail.com>
2026-06-14 17:12:52 +0530

    created groups entity

---
+++ src/main/java/com/sujal/easySettle/entity/Group.java
@@ -0,0 +1,42 @@
+package com.sujal.easySettle.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Table(name = "groups")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Group {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "created_by", nullable = false)
+    private User createdBy;           // who created the group
+
+    @ManyToMany
+    @JoinTable(
+            name = "group_members",      // junction table name
+            joinColumns = @JoinColumn(name = "group_id"),
+            inverseJoinColumns = @JoinColumn(name = "user_id")
+    )
+    private List<User> members;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+}
