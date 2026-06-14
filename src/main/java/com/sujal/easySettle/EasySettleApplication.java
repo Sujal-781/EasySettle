a459fa5391930943f3b4e94594627919e6639df7
Sujal Choudhary <chsujal8@gmail.com>
2026-06-14 16:38:53 +0530

    initial commit

---
+++ src/main/java/com/sujal/easySettle/EasySettleApplication.java
@@ -0,0 +1,13 @@
+package com.sujal.easySettle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasySettleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasySettleApplication.class, args);
+	}
+
+}
