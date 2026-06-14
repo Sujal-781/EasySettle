34834929084d60fd873e6452247497066f40ad0d
Sujal Choudhary <chsujal8@gmail.com>
2026-06-14 17:44:58 +0530

    created GroupRepository

---
+++ src/main/java/com/sujal/easySettle/repository/GroupRepository.java
@@ -0,0 +1,9 @@
+package com.sujal.easySettle.repository;
+
+import com.sujal.easySettle.entity.Group;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GroupRepository extends JpaRepository<Group, Long> {
+}
