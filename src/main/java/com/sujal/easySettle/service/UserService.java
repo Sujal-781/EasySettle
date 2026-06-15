fad766bce3f088cea5f7c750400ec113f7401791
Sujal Choudhary <chsujal8@gmail.com>
2026-06-15 15:41:53 +0530

    created a service class, UserService

---
+++ src/main/java/com/sujal/easySettle/service/UserService.java
@@ -0,0 +1,31 @@
+package com.sujal.easySettle.service;
+
+import com.sujal.easySettle.entity.User;
+import com.sujal.easySettle.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    UserService(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    public User registerUser(User user){
+        userRepository.save(user);
+        return user;
+    }
+
+    public Optional<User> findByEmail(String email){
+        return userRepository.findByEmail(email);
+    }
+
+    public User findById(Long id){
+        return userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("User Not found with id: " + id));
+    }
+}
