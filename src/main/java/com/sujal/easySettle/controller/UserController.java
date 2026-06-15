a4a2e10268f1ae44dd9ba6480671945a628e49fc
Sujal Choudhary <chsujal8@gmail.com>
2026-06-15 16:36:45 +0530

    created UserController with two end points

---
+++ src/main/java/com/sujal/easySettle/controller/UserController.java
@@ -0,0 +1,30 @@
+package com.sujal.easySettle.controller;
+
+import com.sujal.easySettle.entity.User;
+import com.sujal.easySettle.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserService userService;
+
+    UserController(UserService userService){
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<User> registerUser(@RequestBody User user){
+        User savedUser = userService.registerUser(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedUser);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id){
+        User user = userService.findById(id);
+        return ResponseEntity.ok(user);
+    }
+}
