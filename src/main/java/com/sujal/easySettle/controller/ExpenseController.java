1d29f9bea7147880033fb318e1d370c0eadb7379
Sujal Choudhary <chsujal8@gmail.com>
2026-06-16 12:56:26 +0530

    created controller for adding expense

---
+++ src/main/java/com/sujal/easySettle/controller/ExpenseController.java
@@ -0,0 +1,26 @@
+package com.sujal.easySettle.controller;
+
+import com.sujal.easySettle.entity.Expense;
+import com.sujal.easySettle.service.ExpenseService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/expenses")
+public class ExpenseController {
+
+    private final ExpenseService expenseService;
+
+    public ExpenseController(ExpenseService expenseService) {
+        this.expenseService = expenseService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Expense> addExpense(Expense expense){
+        Expense addedExpense = expenseService.addExpense(expense);
+        return ResponseEntity.status(HttpStatus.CREATED).body(addedExpense);
+    }
+}
