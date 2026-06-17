23c59441bb335acea5e1a9246a7c430fc8275de4
Sujal Choudhary <chsujal8@gmail.com>
2026-06-17 19:11:41 +0530

    added new endpoint to get the group balances

--- src/main/java/com/sujal/easySettle/controller/ExpenseController.java
+++ src/main/java/com/sujal/easySettle/controller/ExpenseController.java
@@ -4,10 +4,10 @@ import com.sujal.easySettle.entity.Expense;
 import com.sujal.easySettle.service.ExpenseService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/expenses")
@@ -23,5 +23,10 @@ public class ExpenseController {
     public ResponseEntity<Expense> addExpense(@RequestBody Expense expense){
         Expense addedExpense = expenseService.addExpense(expense);
         return ResponseEntity.status(HttpStatus.CREATED).body(addedExpense);
+    }
+
+    @GetMapping("/groups/{groupId}/balances")
+    public ResponseEntity<Map<Long, BigDecimal>> getGroupBalances(@PathVariable Long groupId){
+        return ResponseEntity.ok(expenseService.getGroupBalances(groupId));
     }
 }
