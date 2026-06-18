2090986cff7937768315c19927bd0e2d9bb337ac
Sujal Choudhary <chsujal8@gmail.com>
2026-06-18 16:23:23 +0530

    added cors Configuration

---
+++ backend/src/main/java/com/sujal/easySettle/config/CorsConfig.java
@@ -0,0 +1,19 @@
+// backend/src/main/java/com/sujal/easySettle/config/CorsConfig.java
+
+package com.sujal.easySettle.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*");
+    }
+}
